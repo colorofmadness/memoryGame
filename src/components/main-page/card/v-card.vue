565cf76406b6928ee0064cf787a09b44e8f83102
vladimir <vladandmir.ya@gmail.com>
2025-02-13 16:27:21 +0500

    add mobile

--- src/components/main-page/card/v-card.vue
+++ src/components/main-page/card/v-card.vue
@@ -1,7 +1,7 @@
 <template>
   <div :class="['card', { 'card--active': active }]">
     <div class="card__front">
-      <v-icon :name="`cards/${icon}`" size="92" />
+      <v-icon :name="`cards/${icon}`" :size="isMobile ? 54 : 92" />
     </div>
     <div class="card__back" />
   </div>
@@ -9,10 +9,12 @@
 
 <script lang="ts" setup>
 import { VIcon } from '@components/ui';
+import { useScreenSize } from '@/composables';
 
 import { defaultProps, ICardProps } from './types';
 
 withDefaults(defineProps<ICardProps>(), defaultProps);
+const { isMobile } = useScreenSize();
 </script>
 
 <style lang="scss" scoped src="./v-card.scss" />
